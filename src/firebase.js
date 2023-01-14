f7072fc28601d44a1ba9157fd545d38d8aa2bb3d
pv-230 <81388791+pv-230@users.noreply.github.com>
2023-01-14 16:05:27 -0500

    Add firebase package and config file

---
+++ src/firebase.js
@@ -0,0 +1,13 @@
+import { initializeApp } from 'firebase/app';
+
+// Not necessary to hide Firebase API key
+const firebaseConfig = {
+  apiKey: 'AIzaSyAcnYaKGUqHjPtC_VSKhqvlKZxG4dS48fM',
+  authDomain: 'where-s-waldo-c4111.firebaseapp.com',
+  projectId: 'where-s-waldo-c4111',
+  storageBucket: 'where-s-waldo-c4111.appspot.com',
+  messagingSenderId: '307823725310',
+  appId: '1:307823725310:web:78d27859dd70f0c9e1f3af',
+};
+
+initializeApp(firebaseConfig);
