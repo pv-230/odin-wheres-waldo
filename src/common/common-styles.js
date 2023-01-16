dc2fc136adaada6add22661657f51113647fa200
pv-230 <81388791+pv-230@users.noreply.github.com>
2023-01-16 15:31:14 -0500

    Update common button style

--- src/common/common-styles.js
+++ src/common/common-styles.js
@@ -10,6 +10,7 @@ const Text = styled.p`
 
 const Button = styled.button.attrs(() => ({ type: 'button' }))`
   background-color: var(--light-color);
+  color: var(--dark-color);
   border: 2px solid var(--dark-color);
   border-radius: 3px;
   padding: 10px 20px;
