fc17aaf5947e10c5f15893dcf958387eaecd5c6f
pv-230 <81388791+pv-230@users.noreply.github.com>
2023-01-17 16:20:42 -0500

    Add scenes data file

---
+++ src/data/scenes.js
@@ -0,0 +1,25 @@
+import OnTheBeach from '../images/scenes/on-the-beach.jpg';
+import SkiSlopes from '../images/scenes/ski-slopes.jpg';
+import SportsStadium from '../images/scenes/sports-stadium.jpg';
+
+const SCENES = [
+  {
+    id: 0,
+    title: 'On the Beach',
+    image: OnTheBeach,
+  },
+  {
+    id: 1,
+    title: 'Ski Slopes',
+    image: SkiSlopes,
+  },
+  {
+    id: 2,
+    title: 'Sports Stadium',
+    image: SportsStadium,
+  },
+];
+
+Object.freeze(SCENES);
+
+export default SCENES;
