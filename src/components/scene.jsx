4380b16072918c766185082fe08fab0be2942471
pv-230 <81388791+pv-230@users.noreply.github.com>
2023-01-18 18:26:38 -0500

    Implement basic image panning

--- src/components/scene.jsx
+++ src/components/scene.jsx
@@ -1,24 +1,71 @@
+import { useState } from 'react';
 import styled from 'styled-components';
 import PropTypes from 'prop-types';
 
 const StyledScene = styled.div`
-  width: 100%;
-  max-width: 100vw;
-  height: 100%;
-  max-height: calc(100vh - 175px);
-  overflow: auto;
+  width: 100vw;
+  height: calc(100vh - 175px);
+  overflow: hidden;
+  background-color: var(--dark-color);
+`;
+
+const SceneImage = styled.img.attrs((props) => ({
+  style: {
+    translate: `${props.translateCoords.x}px ${props.translateCoords.y}px`,
+  },
+}))`
+  transform-origin: 0 0;
 `;
 
 function Scene({ scene }) {
+  const [isPanning, setIsPanning] = useState(false);
+  const [translateCoords, setTranslateCoords] = useState({ x: 0, y: 0 });
+
+  /**
+   * Event handler for starting image panning.
+   * @param {Event} e
+   */
+  function handleMouseDown(e) {
+    e.preventDefault(); // Prevent image dragging
+    setIsPanning(true);
+  }
+
+  /**
+   * Event handler for image panning that updates the image translation offset.
+   * @param {Event} e
+   */
+  function handleMouseMove(e) {
+    if (!isPanning) return;
+    setTranslateCoords((oldCoords) => ({
+      x: oldCoords.x + e.movementX,
+      y: oldCoords.y + e.movementY,
+    }));
+  }
+
+  /**
+   * Event handler for stopping image panning.
+   */
+  function handleMouseUp() {
+    setIsPanning(false);
+  }
+
   return (
     <StyledScene>
-      <img src={scene.image} alt={scene.title} />
+      <SceneImage
+        src={scene.image}
+        alt={scene.title}
+        translateCoords={translateCoords}
+        onMouseDown={handleMouseDown}
+        onMouseMove={handleMouseMove}
+        onMouseUp={handleMouseUp}
+      />
     </StyledScene>
   );
 }
 
 Scene.propTypes = {
   scene: PropTypes.shape({
+    id: PropTypes.number,
     title: PropTypes.string,
     image: PropTypes.string,
   }).isRequired,
