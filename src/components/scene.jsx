f3400df3614fc1472b6c4b127a2e14b517d19d1a
pv-230 <81388791+pv-230@users.noreply.github.com>
2023-01-19 20:01:32 -0500

    Add basic zoom feature

--- src/components/scene.jsx
+++ src/components/scene.jsx
@@ -11,9 +11,10 @@ const StyledScene = styled.div`
   background-color: var(--dark-color);
 `;
 
-const SceneImage = styled.img.attrs(({ translateCoords }) => ({
+const SceneImage = styled.img.attrs(({ translateCoords, scaleVal }) => ({
   style: {
     translate: `${translateCoords.x}px ${translateCoords.y}px`,
+    scale: `${scaleVal}`,
   },
 }))`
   cursor: ${(props) => (props.isPanning ? 'grab' : 'pointer')};
@@ -24,6 +25,7 @@ const SceneImage = styled.img.attrs(({ translateCoords }) => ({
 function Scene({ scene }) {
   const [isPanning, setIsPanning] = useState(false);
   const [translateCoords, setTranslateCoords] = useState({ x: 0, y: 0 });
+  const [scaleVal, setScaleVal] = useState(1);
 
   /**
    * Event handler for starting image panning.
@@ -39,6 +41,12 @@ function Scene({ scene }) {
    * @param {Event} e
    */
   function handleMouseMove(e) {
+    // const rect = e.currentTarget.getBoundingClientRect();
+    // const sceneMouseCoords = {
+    //   x: Math.round(e.clientX - rect.x),
+    //   y: Math.round(e.clientY - rect.y),
+    // };
+    // console.log(sceneMouseCoords);
     if (isPanning) {
       setTranslateCoords((oldCoords) => ({
         x: oldCoords.x + e.movementX,
@@ -54,16 +62,43 @@ function Scene({ scene }) {
     setIsPanning(false);
   }
 
+  /**
+   * Event handler for zooming in or out.
+   */
+  function handleWheel(e) {
+    const ZOOM_INCREMENT = 0.1;
+    const zoomChange = e.deltaY > 0 ? -ZOOM_INCREMENT : ZOOM_INCREMENT;
+    const newZoom = scaleVal + zoomChange;
+
+    setScaleVal(newZoom);
+
+    // const rect = e.currentTarget.getBoundingClientRect();
+
+    // const sceneMouseCoords = {
+    //   x: Math.round(e.clientX - rect.x),
+    //   y: Math.round(e.clientY - rect.y),
+    // };
+    // console.log(`preZoomPoint: ${sceneMouseCoords.x}, ${sceneMouseCoords.y}`);
+
+    // const scaledMouseCoords = {
+    //   x: Math.round((sceneMouseCoords.x / scaleVal) * newZoom),
+    //   y: Math.round((sceneMouseCoords.y / scaleVal) * newZoom),
+    // };
+    // console.log(`postZoomPoint: ${scaledMouseCoords.x}, ${scaledMouseCoords.y}`);
+  }
+
   return (
     <StyledScene>
       <SceneImage
         src={scene.image}
         alt={scene.title}
-        translateCoords={translateCoords}
         isPanning={isPanning}
+        translateCoords={translateCoords}
+        scaleVal={scaleVal}
         onMouseDown={handleMouseDown}
         onMouseMove={handleMouseMove}
         onMouseUp={handleMouseUp}
+        onWheel={handleWheel}
       />
     </StyledScene>
   );
