75d750a0388351fbcfa9a0971289a117f22af8cf
pv-230 <81388791+pv-230@users.noreply.github.com>
2023-01-28 19:52:00 -0500

    Add initial app spinner for loading assets

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,16 +1,25 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import styled from 'styled-components';
 import 'normalize.css';
 
 import GlobalStyle from '../common/global-style';
-import SCENES from '../data/scenes';
+import { Spinner } from '../common/common-styles';
 import StartMenu from './start-menu';
 import Game from './game';
+import scenes from '../data/scenes';
+import {
+  charactersFull,
+  charactersCropped,
+  scenesFull,
+  scenesCropped,
+  icons,
+} from '../data/image-maps';
 
 const StyledApp = styled.div`
   flex: 1;
   display: flex;
   flex-direction: column;
+  justify-content: center;
   align-items: center;
   color: var(--dark-color);
   background-color: var(--light-color);
@@ -19,8 +28,38 @@ const StyledApp = styled.div`
 //-------------------------------------------------------------------------------------------------
 
 function App() {
+  const [gameLoaded, setGameLoaded] = useState(false);
   const [gameStarted, setGameStarted] = useState(false);
-  const [scene, setScene] = useState(SCENES[0]);
+  const [scene, setScene] = useState(scenes[0]);
+
+  /**
+   *
+   */
+  useEffect(() => {
+    const imageMaps = [charactersFull, charactersCropped, scenesFull, scenesCropped, icons];
+    const imageSources = [];
+
+    // Extracts the image sources from the image maps
+    imageMaps.forEach((imageMap) => {
+      const imageArray = Array.from(imageMap);
+      imageArray.forEach((image) => imageSources.push(image[1]));
+    });
+
+    // Waits for all images to fire a load event
+    (async () => {
+      const imagePromises = imageSources.map(
+        (imageSource) =>
+          new Promise((resolve) => {
+            const image = new Image();
+            image.src = imageSource;
+            image.onload = () => resolve(imageSource);
+          })
+      );
+
+      await Promise.all(imagePromises);
+      setGameLoaded(true);
+    })();
+  }, []);
 
   /**
    * Starts the game.
@@ -42,23 +81,24 @@ function App() {
    */
   function handleSceneSelection(e) {
     const sceneVal = Number(e.currentTarget.dataset.scene);
-    setScene(SCENES[sceneVal]);
+    setScene(scenes[sceneVal]);
+  }
+
+  let content;
+  if (gameLoaded) {
+    content = gameStarted ? (
+      <Game scene={scene} stopGame={stopGame} />
+    ) : (
+      <StartMenu scene={scene} handleSceneSelection={handleSceneSelection} startGame={startGame} />
+    );
+  } else {
+    content = <Spinner />;
   }
 
   return (
     <>
       <GlobalStyle />
-      <StyledApp>
-        {gameStarted ? (
-          <Game scene={scene} stopGame={stopGame} />
-        ) : (
-          <StartMenu
-            scene={scene}
-            handleSceneSelection={handleSceneSelection}
-            startGame={startGame}
-          />
-        )}
-      </StyledApp>
+      <StyledApp>{content}</StyledApp>
     </>
   );
 }
